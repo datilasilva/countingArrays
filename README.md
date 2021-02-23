b48a34a51c1a1fe744f984ecb223340fc26482c0
Datila Silva <datilaadv@gmail.com>
2021-02-23 17:41:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+"# countingArrays" 
+"# countingArrays" 
---
+++ script1.js
@@ -0,0 +1,79 @@
+/* Rockeseat Discover Course - Challenge: Searching and counting data with Arrays */
+/* Rockeseat Discover Curso - Desafio:   Procurando e contando dados com Arrays*/
+
+const booksByCategory = [
+    {
+        category:"Riqueza",
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author:"T. Harv Eker",
+            },
+            {
+                title:"O homem mais rico da Babilônia",
+                author:"George S. Clason",
+            },
+            {
+                title:"Pai rico, pai pobre",
+                author:"Robert T Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    },
+    {
+        category: 'Inteligência Emocional',
+        books: [
+            {
+                title: "Você é Insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade - Como enfrentar o mal do século",
+                author:"Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes ",
+                author: "Stephen R. Covey"
+            }
+        ]
+    },
+];
+
+const totalCategories = booksByCategory.length
+
+console.log(totalCategories);
+for(let category of booksByCategory) {
+    console.log('Total Books: ', category.category)
+    console.log(category.books.length)
+}
+
+function countAuthors() {
+    let authors = [];
+
+    for (let category of booksByCategory) {
+        for (let book of category.books) {
+            if(authors.indexOf(book.author) == -1) {
+                authors.push(book.author)
+            }
+        }
+    }
+
+    console.log("Total de autores:", authors.length)
+}
+
+countAuthors();
+
+function booksOfAuthor(author) {
+    let books = [];
+
+    for(let category of booksByCategory) {
+        for(let book of category.books) {
+            if (book.author === author) {
+                books.push(book.title)
+            }
+        }
+    }
+
+    console.log(`Livros do autor ${author}: ${books.join(', ')}`)
+}
+
+booksOfAuthor('Augusto Cury')
